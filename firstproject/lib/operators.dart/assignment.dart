1780ec7a6dc2490ff7a350ee28d9ddd2426bcf62
Jay bhatt <jrbhatt03@gmail.com>
2025-10-12 02:03:23 +0530

    modified comments

--- firstproject/lib/operators.dart/assignment.dart
+++ firstproject/lib/operators.dart/assignment.dart
@@ -1,15 +1,22 @@
 /*void main () {
   double age = 24; // using double as output could be in decimal
+
   age += 6; // age = age + 6
   print("after addition the age is $age");
+
   age -= 3; // age = age - 3
   print("after subtraction the age is $age");
+
   age *= 4; // age = age * 4
   print("after multiplication the age is $age");
+
   age /= 3; // age = age / 3
   print("after division the age is $age");
+  
   age %= 5; // age = age % 5
   print("after modulus the age is $age");
+
+  here value of age is changed after every operation or say updated value is used in next operation
 } */
 
 
@@ -29,7 +36,7 @@ void main () {
   age = original / 3; // age = age / 3
   print("after division the age is $age");
 
-  age = original % 5; // age = age % 5
+  age = original % 5; // age = age % 5 (btw modulus means remainder) so 24/5 = 4.8 ignoring decimal part reamainder is 4 or say 4 * 5 = 20 so 24-20 = 4 the remainder
   print("after modulus the age is $age");
 }
-// you can also use original variable to get original value instead of saving it in another variable
+// used original variable to get original value instead of saving it in another variable
