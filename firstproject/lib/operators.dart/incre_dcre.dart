531b61dafb7ec67637ad6d69b5221b6994c398e8
jrbhatt03-stack <jrbhatt03@gmail.com>
2025-10-11 18:52:31 +0530

    Added a comment to incre_dcre.dart

---
+++ firstproject/lib/operators.dart/incre_dcre.dart
@@ -0,0 +1,8 @@
+void main () {
+  int a = 10;// declaring variable
+  print(++a); // pre-increment operator (output direct:- 1+10=11)
+   
+  int b = 20; // declaring variable
+  print(b++); // post-increment operator (output direct:- 20)
+  print(b);// printing value of b after post-increment (output after direct:- 21)
+}
