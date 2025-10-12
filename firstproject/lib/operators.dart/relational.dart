58e35cc8c5bbfdaf671796a88089b68e5ca72c19
Jay bhatt <jrbhatt03@gmail.com>
2025-10-12 14:34:25 +0530

    learned relational op

---
+++ firstproject/lib/operators.dart/relational.dart
@@ -0,0 +1,10 @@
+void main () {
+  int a = 10; // variable declared
+  int b = 20; // variable declared
+
+  print(a > b); // if a is greater than b then true else false
+  print(a == b); // if a is equal to b then true else false (== means equal to)
+  print(a < b); // if a is less than b then true else false
+  print(a >= b); // if a is greater than or equal to b then true else false
+  print(a <= b); // if a is less than or equal to b then true else false
+}
