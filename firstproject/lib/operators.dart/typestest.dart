fc118e3bf58d2046eea1e34d8a17957c5129c534
Jay bhatt <jrbhatt03@gmail.com>
2025-10-12 15:39:03 +0530

    added a necessary comment

--- firstproject/lib/operators.dart/typestest.dart
+++ firstproject/lib/operators.dart/typestest.dart
@@ -1,11 +1,13 @@
+// ignore_for_file: unnecessary_type_check (have to use it for demonstration purpose)
+
 void main() {
   String name = "jay"; // variable declared
-  int age = 21; // variable declared
-
+  int age = 21; // variable declared 
+  
   print(name is String); // checks if name is String type, output true
   print(age is int); // checks if age is int type, output true
   /* "is" operator is used to check the type of a variable same or not */
-  print(name is !String); // checks if name is not String type, output false
-  print(age is !int); // checks if age is not int type, output false
+  print(name is! String); // checks if name is not String type, output false
+  print(age is! int); // checks if age is not int type, output false
   /* "is!" ie. is not used to check the type of a variable different or not */
 }
