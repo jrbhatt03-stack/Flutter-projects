50e34bd92a61252a5a934e5f9b040a38b2ad7f80
Jay bhatt <jrbhatt03@gmail.com>
2025-10-12 15:34:11 +0530

    learned typetest op

---
+++ firstproject/lib/operators.dart/typestest.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "jay"; // variable declared
+  int age = 21; // variable declared
+
+  print(name is String); // checks if name is String type, output true
+  print(age is int); // checks if age is int type, output true
+  /* "is" operator is used to check the type of a variable same or not */
+  print(name is !String); // checks if name is not String type, output false
+  print(age is !int); // checks if age is not int type, output false
+  /* "is!" ie. is not used to check the type of a variable different or not */
+}
